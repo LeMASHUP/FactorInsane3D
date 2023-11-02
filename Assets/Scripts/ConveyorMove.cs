b6f8513f9308a0bce53ae5068e64efcc6bc3c2e7
Zakaria "LeMASHUP" Dutertre <dutertre.zakaria@gmail.com>
2023-11-02 14:39:31 +0100

    Conveyor system added
    Conveyor system that detect if an object hits the conveyor, it rotate in the direction of the conveyor and starts to go

---
+++ Assets/Scripts/ConveyorMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.transform.rotation = new Quaternion(0, this.transform.rotation.y, 0, 0);
+        other.gameObject.GetComponent<Rigidbody>().velocity = transform.forward * 1;
+    }
+}
