ace1db62ce053cc59342d83c6fca73ebcbd6c553
Zakaria "LeMASHUP" Dutertre <dutertre.zakaria@gmail.com>
2023-11-03 14:41:25 +0100

    Update TextMachineManager
    Now the text change with the machine that is clicked

--- Assets/Scripts/TextMachineManager.cs
+++ Assets/Scripts/TextMachineManager.cs
@@ -1,17 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TextMachineManager : MonoBehaviour
 {
     public string[] machineNames;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public TMP_Text textMachine;
     void Update()
     {
         
